dea3d790bec8e65771ef66c17aaae497b84a14ce
ScottMan1001 <3141996514@qq.com>
2022-12-08 22:58:17 +0800

    fir

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,60 @@
 // index.js
 // 获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: "Video lsit 1",
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData:
+      wx.canIUse("open-data.type.userAvatarUrl") &&
+      wx.canIUse("open-data.type.userNickName"), // 如需尝试获取用户信息可改为false
+
+    videoUrl: "http://localhost:8000/video/1.mp4",
+    everyDayRecommendLabel: "Daily recommendation",
+  },
+  videoErrorCallback: function (e) {
+    console.log("视频错误信息:" + e.detail.errMsg);
+  },
+  screenFullFunc: function (e) {
+    return;
   },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
-      url: '../logs/logs'
-    })
+      url: "../logs/logs",
+    });
   },
   onLoad() {
     if (wx.getUserProfile) {
       this.setData({
-        canIUseGetUserProfile: true
-      })
+        canIUseGetUserProfile: true,
+      });
     }
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: "展示用户信息", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
+        debugger;
+        console.log(res);
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+          hasUserInfo: true,
+        });
+      },
+    });
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+    console.log(e);
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+      hasUserInfo: true,
+    });
+  },
+});
