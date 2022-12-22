19a33a4c00ec2fbda28549c9875a1548ec5f1b6a
ScottMan1001 <3141996514@qq.com>
2022-12-22 16:02:50 +0800

    appinit

---
+++ components/upload/upload.js
@@ -0,0 +1,57 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    fileList: [],
+  },
+
+  chooseImg: function (e) {
+    var that = this;
+    var fileList = this.data.fileList;
+    wx.chooseMedia({
+      mediaType: ["video"],
+      count: 1,
+      // sizeType: ["original", "compressed"], // 可以指定是原图还是压缩图，默认二者都有
+      // mediaType: ["mix"],
+      sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFiles.forEach((item) => {
+          if (item.size > 41943040) {
+            wx.showToast({
+              title: "视频过大！",
+              icon: "error",
+              mask: true,
+              duration: 2000,
+            });
+            return;
+          }
+          if (item.fileType != "video" && !item.tempFilePath.includes(".mp4")) {
+            wx.showToast({
+              title: "支持video/MP4",
+              icon: "error",
+              mask: true,
+              duration: 2000,
+            });
+            return;
+          }
+          fileList.push({ url: item.tempFilePath, type: item.fileType });
+        });
+
+        that.setData({
+          fileList: fileList,
+        });
+        console.log(that.data.fileList);
+        that.triggerEvent("getUrl", { fileList: fileList });
+      },
+    });
+  },
+
+  deleteImg(e) {
+    this.data.imgs.splice(e.currentTarget.dataset.index, 1);
+    this.setData({
+      imgs: this.data.imgs,
+    });
+  },
+});
--- pages/records/records.js
+++ pages/records/records.js
@@ -1,6 +1,57 @@
 Page({
   data: {
     text: "Developing",
+    showFlag: true,
   },
+  getUrl({ detail }) {
+    const filePath = detail.fileList[0].url;
+    console.log(detail.fileList[0].url);
+
+    const uploadTask = wx.uploadFile({
+      url: "http://192.168.0.101:8090/fileVideo/fileUpload", //仅为示例，非真实的接口地址
+      filePath: filePath,
+      name: "file",
+
+      success(res) {
+        const data = res.data;
+        //do something
+      },
+      fail(e) {
+        wx.showToast({
+          title: "Fail！",
+          icon: "error",
+          duration: 1500,
+        });
+        console.log(e);
+      },
+    });
+    uploadTask.onProgressUpdate((res) => {
+      if (res.progress < 100) {
+        wx.showToast({
+          title: "Uploading！",
+          icon: "loading",
+        });
+      } else {
+        wx.showToast({
+          title: "Uploaded！",
+          icon: "success",
+        });
+      }
+      console.log("上传进度", res.progress);
+      console.log("已经上传的数据长度", res.totalBytesSent);
+      console.log("预期需要上传的数据总长度", res.totalBytesExpectedToSend);
+    });
+  },
+
   onLoad() {},
+  onHide() {
+    this.setData({
+      showFlag: false,
+    });
+  },
+  onShow() {
+    this.setData({
+      showFlag: true,
+    });
+  },
 });
